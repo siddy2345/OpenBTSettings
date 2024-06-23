fbac6c84ea903f6062ac65753df76fa283add7e7
Sahid Fakiry <s.fakiry23@gmail.com>
2024-06-23 18:17:25 +0200

    Add project files.

---
+++ RunBtBatFile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace RunBtBatFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteFile(@"%windir%\explorer.exe ms-settings:bluetooth");
+        }
+
+        public static void ExecuteFile(string command)
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo(@"cmd.exe", "/c" + command);
+            Process process;
+
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+
+            process = Process.Start(processInfo);
+
+            process.Close();
+        }
+    }
+}
